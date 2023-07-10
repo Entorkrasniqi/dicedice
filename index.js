2bcf315366b527142a18e1620765c3e69d21e504
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2023-07-10 17:06:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var random = math.random();
-randomNumber = random*6;
-randomNumber1 = math.floor(randomNumber) + 1;
+var randomNum1=Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+
